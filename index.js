fafccf5031b4264797090f7d8b99c4220cfc4150
SHEHANA-PAREETH <shehanapareeth@gmail.com>
2024-04-23 00:38:44 +0530

    new

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send('hiiii')
 })
 app.use('/api/users',userRoutes)
