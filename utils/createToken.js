24d966ff8433e2317800331fc4d1108363a1209d
SHEHANA-PAREETH <shehanapareeth@gmail.com>
2024-04-24 12:03:56 +0530

    new

--- utils/createToken.js
+++ utils/createToken.js
@@ -5,13 +5,18 @@ const generateToken=(res,userId)=>{
         expiresIn: "30d",
     })
   
-res.cookie('jwt',token,{
+/*res.cookie('jwt',token,{
         httpOnly: true,
         secure: process.env.NODE_ENV === "development",
        sameSite : "strict",
        maxAge: 30*24*60*60*1000
-    })
-
+    })*/
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        sameSite: "Lax",
+        maxAge: 30 * 24 * 60 * 60 * 1000
+      });
+      
     return token
 }
 module.exports = generateToken
